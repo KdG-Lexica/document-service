c3242502dd0423fa61483b592348004101f97dba
Quinten Verhelst <quinten.verhelst@student.kdg.be>
2022-02-14 12:56:26 +0100

    feat: 🎸 Add Redis cache layer

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,42 @@
 import mongoose from 'mongoose';
-// const MONGODB_URL = process.env.MONGODB_URL;
-console.log(process.env.MONGODB_URL);
-export default mongoose.connect(process.env.MONGODB_URL, { dbName: 'nyt', user: 'root', pass: 'example' });
+const MONGODB_URL = process.env.MONGODB_URL;
+
+import { createClient } from 'redis'
+
+export const applyMongooseCache = async () => {
+
+  const client = createClient()
+  await client.connect()
+  const exec = mongoose.Query.prototype.exec
+
+  mongoose.Query.prototype.exec = async function () {
+    const key = JSON.stringify(
+      {
+        ...this.getQuery(),
+        collection: this.mongooseCollection.name,
+        op: this.op,
+        options: this.options
+      }
+    )
+      
+    const cacheValue = await client.get(key)
+
+    if (cacheValue){
+      console.log('Cache HIT')
+      return JSON.parse(cacheValue)
+    }
+    console.log('Cache MISS')
+
+    const result = await exec.apply(this, arguments)
+
+    if (result) {
+      await client.set(key, JSON.stringify(result))
+    }
+    return result
+  }
+}
+
+const connection = mongoose.connect(MONGODB_URL, { dbName: "nyt", user: "root", pass: "example" });
+
+export default connection;
 /* export const db = mongoose.connection; */
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as cors from 'cors';
 import { HandleError } from './controllers/ErrorController';
 import { HttpException } from './exceptions/HttpException';
 import 'dotenv/config'
-import db from './db';
+import db, { applyMongooseCache } from './db';
 import ArticleRoute from './routes/ArticleRoutes';
 const app = express();
 const PORT = process.env.PORT as unknown as number || 3000;
@@ -13,6 +13,31 @@ const PORT = process.env.PORT as unknown as number || 3000;
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cors.default());
+
+const getDurationInMilliseconds = (start: any) => {
+  const NS_PER_SEC = 1e9
+  const NS_TO_MS = 1e6
+  const diff = process.hrtime(start)
+
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+}
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl} [STARTED]`)
+  const start = process.hrtime()
+
+  res.on('finish', () => {
+      const durationInMilliseconds = getDurationInMilliseconds (start)
+      console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds.toLocaleString()} ms`)
+  })
+
+  res.on('close', () => {
+      const durationInMilliseconds = getDurationInMilliseconds (start)
+      console.log(`${req.method} ${req.originalUrl} [CLOSED] ${durationInMilliseconds.toLocaleString()} ms`)
+  })
+
+  next()
+})
 
 
 app.get('/api/health', (req: Request, res: Response) => {
@@ -27,10 +52,12 @@ app.use((error: HttpException, req: Request, res: Response, next: NextFunction) 
   HandleError(error, req, res, next)
 });
 
-
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`API running on port ${PORT}!`);
-  });
+applyMongooseCache().then(() => {
+  db.then(() => {
+    app.listen(PORT, () => {
+      console.log(`API running on port ${PORT}!`);
+    });
+  })
 })
+
 
