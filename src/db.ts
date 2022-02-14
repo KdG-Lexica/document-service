735e0759355868732c477892bdf8f29870da73e8
Quinten Verhelst <quinten.verhelst@student.kdg.be>
2022-02-14 13:01:34 +0100

    fix: 🐛 Only connect redis in production

--- src/db.ts
+++ src/db.ts
@@ -1,39 +1,42 @@
 import mongoose from 'mongoose';
 const MONGODB_URL = process.env.MONGODB_URL;
+const IS_PRODUCTION = process.env.enviroment === 'prod';
 
 import { createClient } from 'redis'
 
 export const applyMongooseCache = async () => {
+  if (IS_PRODUCTION) {
+    const client = createClient()
+    await client.connect()
+    const exec = mongoose.Query.prototype.exec
 
-  const client = process.env.enviroment === 'prod' ? createClient({ url: 'redis://redis'})  :createClient()
-  await client.connect()
-  const exec = mongoose.Query.prototype.exec
+    mongoose.Query.prototype.exec = async function () {
+      const key = JSON.stringify(
+        {
+          ...this.getQuery(),
+          collection: this.mongooseCollection.name,
+          op: this.op,
+          options: this.options
+        }
+      )
 
-  mongoose.Query.prototype.exec = async function () {
-    const key = JSON.stringify(
-      {
-        ...this.getQuery(),
-        collection: this.mongooseCollection.name,
-        op: this.op,
-        options: this.options
+      const cacheValue = await client.get(key)
+
+      if (cacheValue) {
+        console.log('Cache HIT')
+        return JSON.parse(cacheValue)
       }
-    )
-      
-    const cacheValue = await client.get(key)
+      console.log('Cache MISS')
 
-    if (cacheValue){
-      console.log('Cache HIT')
-      return JSON.parse(cacheValue)
+      const result = await exec.apply(this, arguments)
+
+      if (result) {
+        await client.set(key, JSON.stringify(result))
+      }
+      return result
     }
-    console.log('Cache MISS')
-
-    const result = await exec.apply(this, arguments)
-
-    if (result) {
-      await client.set(key, JSON.stringify(result))
-    }
-    return result
   }
+
 }
 
 const connection = mongoose.connect(MONGODB_URL, { dbName: "nyt", user: "root", pass: "example" });
