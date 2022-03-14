bf7e39d128021c02434132991b158e2eadc0c54d
Quinten Verhelst <quinten.verhelst@student.kdg.be>
2022-03-14 15:22:59 +0100

    do not run this, deployment WIP

--- src/db.ts
+++ src/db.ts
@@ -5,10 +5,10 @@ import { Sequelize } from "sequelize";
 
 // SQL SETUP
 const config = {
-  host: "141.94.222.96",
-  user: "root",
-  database: "lexica",
-  password: "watchjs",
+  host: process.env.production === 'yes' ? 'mysql' : "141.94.222.96",
+  user: process.env.production === 'yes' ? process.env.MYSQL_USER : "root",
+  database: process.env.production === 'yes' ? process.env.DATABASE : "lexica",
+  password: process.env.production === 'yes' ? process.env.MYSQL_PASSWORD : "watchjs",
 };
 
 let queryCount = 0;
@@ -43,8 +43,11 @@ IPTCSet.hasMany(IPTCMeta);
 
 
 // MONGO SETUP
-const MONGODB_URL = process.env.MONGODB_URL;
-const connectMongo = () => { return mongoose.connect(MONGODB_URL, { dbName: "nyt", user: "root", pass: "example" }) }
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://mongodb.verhelst.dev:27017";
+const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME || 'nyt' 
+const MONGODB_USER = process.env.MONGO_USERNAME || 'root';
+const MONGODB_PASSWORD = process.env.MONGO_PASSWORD || 'example'
+const connectMongo = () => { return mongoose.connect(MONGODB_URL, { dbName: MONGODB_DB_NAME, user: MONGODB_USER , pass: MONGODB_PASSWORD }) }
 mongoose.set('debug', { shell: true })
 export {
   connectMongo,
